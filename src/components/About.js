8ded6bf4e260ae60bcb9677793d2ffc110edd2cd
EdoardoCremaschi <edoardo.cremaschi@gmail.com>
2021-08-26 16:54:48 +0200

    link fix.

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About(){
         <div className='about-text'>
         <p>A Nuclear Manatee swims between liminal places, collecting the fragments that everyday life loses trough friction in a ephemeral orbit.</p>
         <p>He then seeks refuge in a glowing reef and use those fragments to make indie tabletop roleplaying games <strong>built around powerful questions</strong>.</p>
-        <p>Here, you can either see <strong><Link exact to='/newsletter'>the process</Link></strong> or <strong><Link exact to='/games'>the result</Link></strong></p>
+        <p>Here, you can either see <strong><a target='_blank' rel='noreferrer' href='https://mailchi.mp/2c01d85709bb/a-message-in-the-bottle'>the process</a></strong> or <strong><Link exact to='/games'>the result</Link></strong></p>
         <p>
 
         </p>
